fc8ad3b7b57fd76bc7d69f30c301066caeb075ec
alireza.dizaji <alireza.dizaji@cn-a010.server.mila.quebec>
2024-06-19 14:52:59 -0400

    Fix

--- tse/utils/pick_best_res.py
+++ tse/utils/pick_best_res.py
@@ -2,6 +2,8 @@ import json
 import math
 from sys import argv
 
+import numpy as np
+
 if __name__ == "__main__":
     dir = argv[1]
     metric = argv[2]
@@ -11,15 +13,20 @@ if __name__ == "__main__":
         data = json.load(f)
 
 
-    best_metric = -math.inf
+    metric_val = np.zeros(len(data))
     best_config = None
 
-    for d in data:
-        if d[metric] >= best_metric:
-            best_metric = d[metric]
-            best_config = d
-    
+    for i, d in enumerate(data):
+        metric_val[i] = d[metric]
+
+    best_exp = np.argmax(metric_val)
+    best_config = data[best_exp]
+
+    print(f"### results sorted {np.sort(metric_val)}")
     print("@@@@ Best configuration setup @@@@", flush=True)
     for h in hyperparameters:
         print(f"{h}: {best_config[h]}", flush=True)
-    print(f"{metric}: {best_metric}", flush=True)
+    print(f"{metric}: {metric_val[best_exp]}", flush=True)
+
+    num_epochs = len(best_config['train_times'])
+    print(f"Num of trained epochs: {num_epochs}")
